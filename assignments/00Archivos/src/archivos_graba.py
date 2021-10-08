5f2f4724f2971bdf3e4fee672095ecbf6bb95b61
samlopezv <a01742394@itesm.mx>
2021-10-08 02:48:28 +0000

    Proyecto

--- assignments/00Archivos/src/archivos_graba.py
+++ assignments/00Archivos/src/archivos_graba.py
@@ -1,4 +1,5 @@
-def imc(nombre):
+def imc(nombre, num):
+    print('\n')
     print('Calculadora de IMC')
     peso = float(input('Ingresa tu peso en kg: '))
     estatura = float(input('Ingresa tu estatura en metros: '))
@@ -21,27 +22,47 @@ def imc(nombre):
         print('Lo que significa que tienes obesidad III')
     else:
         print('Lo que significa que tienes obesidad IV')
+    print('\n')
+    print('IMC= 1     Gráficas=2     Salir=3')
+    numnuevo = int(input())
+    return(nombre, numnuevo)
+     
 
 def graficas():
-    
-
+    x =[]
+    y1=[]
+    with open('assignments/00Archivos/data/Obesidad en méxico.csv', 'r') as f:
+        for column in f:
+            lista_column = column.split(',')
+            x.append(lista_column[f])
+        print(x)
 
 
 def main():
     nombre = (input('Ingresa tu nombre: '))
+    print('\n')
     print(f'¡Hola {nombre}! ¿qué deseas conocer?')
     print('IMC= 1     Gráficas=2     Salir=3')
     num = int(input())
-    if num == 1:
-        imc(nombre)
-        nombre = num
-    elif num == 2:
-        print('Gráficas')
-    elif num == 3:
-        print('Gracias por tu visita')
-    else:
-        #editar para que siga pidiendo y redirija
+    while num != 3:
+        if num == 1:
+            variableimc = imc(nombre, num)
+            num = variableimc[1]
+        elif num == 2:
+            graficas()
+        else:
             print('Favor de ingresar un dato válido')
+            num = int(input())
+            if num != 1 or num !=2 or num !=3:
+                num = num
+            else:
+                num = 4
+            while num == 4:
+                print('Favor de ingresar un dato válido')
+                num = int(input())
+        
+    print('Gracias por tu visita')
+
 
 
 if __name__=='__main__':
